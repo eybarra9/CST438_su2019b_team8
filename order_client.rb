26554f64e541cd92fec48c95dc1b346a17d0cad1
eybarra9 <erybarra@csumb.edu>
2019-08-14 00:54:04 +0000

    Add most of code necessary for order client

--- order_client.rb
+++ order_client.rb
@@ -55,14 +55,61 @@ class ItemClient
 end
 
 while true
-    puts "What would you like to do: (1) Create a new order, (2) retreive an existing order, (3) register a new customer, 
-          (4) lookup a customer, (5) create a new item, (6) lookup an item or (7) quit ?"
+    puts "What would you like to do:  (1) Create a new order, 
+                            (2) retreive an existing order, 
+                            (3) register a new customer, 
+                            (4) lookup a customer, 
+                            (5) create a new item, 
+                            (6) lookup an item or 
+                            (7) quit ?"
     input = gets.chomp!
     
     case input
-        when '7' || 'quit'
-            break
 
+        when '1'
+            puts 'Enter an item ID'
+            itemId = gets.chomp!
+            puts 'Enter an email'
+            email = gets.chomp!
+            response = OrderClient.create itemId: itemId, email: email
+            puts "status code #{response.code}"
+
+        when '2'
+            puts 'Enter an ID of the order to search for'
+            id = gets.chomp!
+            response = OrderClient.getId(id)
+            puts "status code #{response.code}"
+            
+        when '3'
+            puts 'To register a customer please enter the lastname, firstname and email separated by a blank space'
+            input = gets.chomp!.split()
+            response = CustomerClient.register lastName: input[0], firstName: input[1], email: input[2]
+            puts "status code #{response.code}"
+            
+        when '4'
+            puts 'To lookup a customer enter a customer ID or email'
+            input = gets.chomp!
+            if input.include?('@')
+                response = CustomerClient.getEmail(input)
+            else 
+                response = CustomerClient.getId(input)
+            end 
+            puts "status code #{response.code}"
+            
+        when '5'
+            puts "To create an item enter item description, price and stockQty all separated by a black space"
+            input = gets.chomp!.split()
+            response = ItemClient.create description: input[0], price: input[1], stockQty: input[2]
+            puts "status code #{response.code}"
+            
+        when '6'
+            puts "To look up an item enter the ID of item to search for"
+            input = gets.chomp!
+            response = ItemClient.getId(id)
+            puts "status code #{response.code}"
+            
+        when '7'
+            break
             
     end
 end
